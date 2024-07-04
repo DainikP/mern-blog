9e6f61de07c1a4d9b02ea23a89810c490cf458db
dainik <dainik.prajapati@smartters.in>
2024-07-04 11:16:11 +0530

    create page and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Dashbord from "./pages/Dashbord"
+import Signin from "./pages/Signin"
+import Signup from "./pages/SignUp"
 
 function App() {
-
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<Home />}></Route>
+     <Route path="/about" element={<About />}></Route>
+     <Route path="/sign-in" element={<Signin />}></Route>
+     <Route path="/sign-up" element={<Signup />}></Route>
+     <Route path="/projects" element={<Projects />}></Route>
+     <Route path="/dashbord" element={<Dashbord />}></Route>
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
