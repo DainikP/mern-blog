08da49b9116d8333b53139c028c39477286385b2
dainik <dainik.prajapati@smartters.in>
2024-07-05 08:56:36 +0530

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+})
 
 
 app.listen(3000, () => {
